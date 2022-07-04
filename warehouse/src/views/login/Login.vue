3bd0f0b3bc69b9b4cd8fc06582c7702eee4f3471
刘嘉晨 <9869146+Poetry-Meam@user.noreply.gitee.com>
2022-07-04 10:00:53 +0800

    login-bug

--- warehouse/src/views/login/Login.vue
+++ warehouse/src/views/login/Login.vue
@@ -1,11 +1,43 @@
 <template>
-    
+    <div id="login">
+        <canvas id="canvas"></canvas>
+    </div>
 </template>
 
 <script setup>
-
+import { reactive, ref, onMounted } from 'vue';
+    // 登录页背景
+    const canvasBg = () => {
+        var bodywidth = document.body.clientWidth;
+        var bodyheight = document.body.clientHeight; 
+        var canvas = document.getElementById('canvas');
+        console.log(canvas.getContext)
+        canvas.width = bodywidth;
+        canvas.height = bodyheight;
+        if(!canvas.getContext) return;
+        var background = canvas.getContext('2d');
+        background.strokeStyle = 'rgb(242,245,251)';
+        for(var y = 0; y < canvas.height/30; y++){
+            for(var x = 0; x < canvas.width/30;x++){
+                background.strokeRect(x*30, 1+y*30, 30,30);
+            }
+        }
+    }
+    // 生命周期:挂载完数据
+    onMounted(() => {
+        canvasBg();
+    })
 </script>
 
-<style scoped lang='less'>
+<style scoped>
+#login{
+  width: 100%;
+  height: 100vh;
 
+}
+#canvas{
+    position: absolute;
+    top: 0;
+    z-index:-1;
+}
 </style>
